58497803b95bdc27725be912e300962a5f7ffa17
kanamycine <jinyeong5320@gmail.com>
2022-10-25 21:30:18 +0900

    fix : repository seller 추가되어있는 부분 분리 - mailUtil 에러 : monolith에서 가져와서 수정 - S3 Amazon 에러 : 보류

--- src/main/java/com/team6/onandthefarmproductservice/repository/ProductPagingRepository.java
+++ src/main/java/com/team6/onandthefarmproductservice/repository/ProductPagingRepository.java
@@ -11,47 +11,47 @@ import com.team6.onandthefarmproductservice.entity.Product;
 
 public interface ProductPagingRepository extends PagingAndSortingRepository<Product, Long> {
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+	@Query(value = "select p from Product p join fetch p.category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findAllProductOrderByNewest(PageRequest pageRequest);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+	@Query(value = "select p from Product p join fetch p.category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductListByHighPrice(PageRequest pageRequest);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+	@Query(value = "select p from Product p join fetch p.category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductListByLowPrice(PageRequest pageRequest);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+	@Query(value = "select p from Product p join fetch p.category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductBySoldCount(PageRequest pageRequest);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.seller.sellerId =:sellerId and p.productStatus ='selling'",
+	@Query(value = "select p from Product p join fetch p.category where p.sellerId =:sellerId and p.productStatus ='selling'",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductBySellerNewest(PageRequest pageRequest, @Param("sellerId") Long sellerId);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.seller.sellerId =:sellerId and p.productStatus ='selling'",
+	@Query(value = "select p from Product p join fetch p.category where p.sellerId =:sellerId and p.productStatus ='selling'",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findSellingProductBySellerNewest(PageRequest pageRequest, @Param("sellerId") Long sellerId);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.seller.sellerId =:sellerId and p.productStatus ='pause'",
+	@Query(value = "select p from Product p join fetch p.category where p.sellerId =:sellerId and p.productStatus ='pause'",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findPauseProductBySellerNewest(PageRequest pageRequest, @Param("sellerId") Long sellerId);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+	@Query(value = "select p from Product p join fetch p.category where p.category.categoryName =:category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductsByCategoryNewest(PageRequest pageRequest, @Param("category") String category);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+	@Query(value = "select p from Product p join fetch p.category where p.category.categoryName =:category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductByCategoryHighest(PageRequest pageRequest, @Param("category") String category);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+	@Query(value = "select p from Product p join fetch p.category where p.category.categoryName =:category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductByCategoryLowest(PageRequest pageRequest, @Param("category") String category);
 
-	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+	@Query(value = "select p from Product p join fetch p.category where p.category.categoryName =:category",
 			countQuery = "select count(p) from Product p")
 	Page<Product> findProductByCategorySoldCount(PageRequest pageRequest, @Param("category") String category);
 }
--- src/main/java/com/team6/onandthefarmproductservice/util/MailUtils.java
+++ src/main/java/com/team6/onandthefarmproductservice/util/MailUtils.java
@@ -14,21 +14,21 @@ public class MailUtils {
     private MimeMessage message;
     private MimeMessageHelper messageHelper;
 
-    public MailUtils(JavaMailSender mailSender) throws MessagingException {
+    public MailUtils(JavaMailSender mailSender) throws javax.mail.MessagingException {
         this.mailSender = mailSender;
         message = this.mailSender.createMimeMessage();
         messageHelper = new MimeMessageHelper(message, true, "UTF-8");
     }
 
-    public void setSubject(String subject) throws MessagingException {
+    public void setSubject(String subject) throws javax.mail.MessagingException {
         messageHelper.setSubject(subject);
     }
 
-    public void setText(String htmlContent) throws MessagingException {
+    public void setText(String htmlContent) throws javax.mail.MessagingException {
         messageHelper.setText(htmlContent, true);
     }
 
-    public void setFrom(String email, String name) throws UnsupportedEncodingException, MessagingException {
+    public void setFrom(String email, String name) throws UnsupportedEncodingException, javax.mail.MessagingException {
         messageHelper.setFrom(email, name);
     }
 
