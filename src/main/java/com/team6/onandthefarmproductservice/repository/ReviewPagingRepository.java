770c5ced34cca80c15d3497f132e77a1c096b491
ksh940925 <ksh9409255@naver.com>
2022-11-03 16:19:33 +0900

    fix: @Request param으로 변경(@RequestBody를) / feignClient부분의 rul 수정

--- src/main/java/com/team6/onandthefarmproductservice/repository/ReviewPagingRepository.java
+++ src/main/java/com/team6/onandthefarmproductservice/repository/ReviewPagingRepository.java
@@ -9,15 +9,15 @@ import org.springframework.data.repository.query.Param;
 import com.team6.onandthefarmproductservice.entity.Review;
 
 public interface ReviewPagingRepository extends PagingAndSortingRepository<Review, Long> {
-	@Query(value = "select r from Review r join fetch r.product p join fetch p.category join fetch p.seller where r.product.productId =:productId and r.reviewStatus ='created'",
+	@Query(value = "select r from Review r join fetch r.product p join fetch p.category where r.product.productId =:productId and r.reviewStatus ='created'",
 			countQuery = "select count(r) from Review r where r.product.productId =:productId and r.reviewStatus ='created'")
 	Page<Review> findReviewListOrderBy(PageRequest pageRequest, @Param("productId") Long productId);
 
-	@Query(value = "select r from Review r join fetch r.seller s where r.seller.sellerId =:sellerId and r.reviewStatus ='created'",
-			countQuery = "select count(r) from Review r where r.seller.sellerId =:sellerId and r.reviewStatus ='created'")
+	@Query(value = "select r from Review r where r.sellerId =:sellerId and r.reviewStatus ='created'",
+			countQuery = "select count(r) from Review r where r.sellerId =:sellerId and r.reviewStatus ='created'")
 	Page<Review> findReviewListBySeller(PageRequest pageRequest, @Param("sellerId") Long sellerId);
 
-	@Query(value = "select r from Review r join fetch r.user u where r.user.userId =:userId and r.reviewStatus ='created'",
-			countQuery = "select count(r) from Review r where r.user.userId =:userId and r.reviewStatus ='created'")
+	@Query(value = "select r from Review r where r.userId =:userId and r.reviewStatus ='created'",
+			countQuery = "select count(r) from Review r where r.userId =:userId and r.reviewStatus ='created'")
 	Page<Review> findReviewListByUser(PageRequest pageRequest, @Param("userId") Long userId);
 }
