9b9583139ed190a4cfa1a5149ea1072a3a19bd39
ksh940925 <ksh9409255@naver.com>
2022-11-08 11:53:28 +0900

    fix: 멱등 수신자를 위한 로직 수정

--- src/main/java/com/team6/onandthefarmproductservice/repository/ReservedOrderRepository.java
+++ src/main/java/com/team6/onandthefarmproductservice/repository/ReservedOrderRepository.java
@@ -4,10 +4,13 @@ import com.team6.onandthefarmproductservice.entity.ReservedOrder;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservedOrderRepository extends CrudRepository<ReservedOrder,Long> {
 
     List<ReservedOrder> findByOrderSerialAndIdempoStatus(String orderSerial, Boolean status);
 
     boolean existsByReservedOrderIdAndIdempoStatus(Long id, Boolean status);
+
+    Optional<ReservedOrder> findByOrderSerial(String orderSerial);
 }
